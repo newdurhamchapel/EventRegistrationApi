4b90cd4c9822ed3ae7bd86efd727771234d3a154
codedev@newdurhamchapel.org <codedev@newdurhamchapel.org>
2017-03-25 11:33:54 -0400

    testing

---
+++ newduram_api_start/src/newduram_api_start/newduram_api.java
@@ -0,0 +1,123 @@
+package newduram_api_start;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import org.apache.log4j.Logger;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+public class newduram_api {
+
+	
+	public static void main(String[] args) {
+		
+		/* String test = getoutput("Henry");
+		
+		System.out.println(test);
+		System.out.println(getoutput("Tom"));
+		System.out.println(getoutput("Jerry"));
+		System.out.println(getSum(1,2)); */
+		
+		String user = "ab1fc474b17b5c167f70758e7b4b5ee1917a119e4ca49f66571578f0ccf1eb47";
+		String token = "7a36cd7c3bb11343c4282d2c601b7003f455881b8c866e1f09b135a30967e3db";
+		String Planningurl = "https://api.planningcenteronline.com/people/v2/people";
+		
+		String encodedauth = Base64.getEncoder().encodeToString((user+":"+ token).getBytes(StandardCharsets.UTF_8));  
+		
+		try {
+			URL url = new URL(Planningurl);
+			HttpURLConnection connection = null;
+			try {
+				connection = (HttpURLConnection) url.openConnection();
+				connection.setRequestMethod("GET");
+				connection.setDoOutput(true);
+				connection.setRequestProperty("Authorization", "Basic "+encodedauth);
+				connection.connect();
+		
+				InputStream stream = connection.getInputStream();
+				
+				 BufferedReader rd = new BufferedReader(new InputStreamReader(stream));
+				    StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
+				    String line;
+				    while ((line = rd.readLine()) != null) {
+				      response.append(line);
+				      response.append('\r');
+				    }
+				    rd.close();
+				
+				 stream.close();
+				 
+				 
+				System.out.print(response);
+				 
+				 
+				ObjectMapper object = new ObjectMapper();
+				
+				JsonNode jsonObject = object.readTree(response.toString());
+				
+				System.out.println(jsonObject.get("data"));
+				 
+
+				 
+				
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				connection.disconnect();
+			}
+			
+			
+
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
+		
+	} // Main Loop
+	
+	
+	public static String getoutput(String name) {
+		
+		String output = "Hello " + name; 
+		
+		return output;
+		
+		
+	} // Function getoutput
+	
+	public static int getSum(int first,int second) {
+		int output = first + second;
+		
+		return output;
+		
+	}
+	
+	public static String getforminfo(String FirstName,String LastName,String EmailAddress,Boolean Contact) {
+	
+	String output = "";
+		
+	return output;
+	}
+	
+	
+	
+	
+	
+
+} // Class newdurham_api
+
+//This is a great program
